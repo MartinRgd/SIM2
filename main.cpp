fc97707910e9ae553f4eb4c14896e701ee33d276
ZHONGXIUZHANG <zzxshr@sina.cn>
2017-03-02 21:52:21 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,123 +1,148 @@
-#include<iostream>
-#include<vector>
-
+#include <iostream>
+#include <vector>
+#include <list>
+#include <cstdlib>
+#include <cmath>
 #include"noeud.h"
-#include "matrice_creuse.h"
-#include "grad_conj.h"
-#include "triangles.h"
-#include "trin.h"
-#include "intP2.h"
+#include"matrice_creuse.h"
+#include"triangles.h"
+#include"trin.h"
+#include"assemblage.h"
+#include"intP2.h"
 
 using namespace std;
 
+
+
 int main()
 {
-    /*Tests sur les fonctions de bases des classes Noeud et Matrice_creuse */
-    {
-        cout << "Test des fonctions de base" << endl;
-        Noeud N1(1,1,2.);
-        Noeud N2(2,2,3.);
-        Noeud N3(1,2,2.);
+    //test de la part de matrice
 
+        Noeud N1(0,1,2.);
+        Noeud N2(2,1,3.);
 
         Matrice_creuse M(3,3);
-        M.get_Chaine().push_back(N1);
-        M.get_Chaine().push_back(N2);
-        M.get_Chaine().push_back(N3);
+        M.Chaine.push_back(N1);
+        M.Chaine.push_back(N2);
 
         M.print();
-        cout << "Fin print" << endl;
-
+        cout << "fin print" << endl;
         vector<double> V(3);
         vector<double> R(3);
         V.at(0) = 1.;
-        V.at(1) = 1.;
+        V.at(1) = 2.;
         V.at(2) = 1.;
+        cout << "ok" << endl;
         R = M*V;
-        print(R);
+        cout <<"Res : "<< R.at(0) << "," << R.at(1) << "," << R.at(2) << endl;
+
+        Noeud NN1(0,1,2.);
+        Noeud NN2(2,2,4.);
+
+        Matrice_creuse M1(3,3);
+        M1.Chaine.push_back(NN1);
+        M1.Chaine.push_back(NN2);
+        M1.print();
+        //cout<<M1.get_val(2,2)<<endl;
+
+        cout<<"partie de matrice_creuse ok"<<endl;
+
+    //test de la part d'integrale
+
+    point n1(1,0.,0.);
+    point n2(2,1.,0.);
+    point n3(3,0.,1.);
+    cout<<n3.y<<endl;
+    //triangle tr(n1,n2,n3);
+    //point test;
+    //test=tr.sommets[0];
+   // cout<<test.y;
+    //cout<<tr.sommets[0].y<<endl;
+    Triinte ti(n1,n2,n3);
+    cout<<ti.sommets[2].y<<endl;
+    cout<<"yeah"<<endl;
 
 
-        cout << " Test de la fonction diag" << endl;
-        vector<double> Vec(3);
-        Vec = diag(M);
-        print(Vec);
+    cout<<ti.bl<<endl;
+    cout<<ti.sommets[0].y;
+    ti.masse();
+    ti.mprint();
+    ti.rigid();
+    ti.rprint();
 
-        cout << "Fin Test des fonctions de base" << endl;
-    }
-    /* Tests sur le gradient conjugué préconditionné */
-    {
-        cout << "Test du gradient conjugué" << endl;
-        // Si A est la matrice identité on doit avoir x = b
-        cout << "Test identité" << endl;
-        Matrice_creuse A(2,2);
-        Noeud N1(1,1,1.);
-        Noeud N2(2,2,1.);
-        A.get_Chaine().push_back(N1);
-        A.get_Chaine().push_back(N2);
+    cout<<"partie d'integrale 1 ok"<<endl;
 
-        vector<double> b(2,2.);
-        vector<double> x=grad_conj(A,b);
-        print(x);
+    //test de la part assemblage
 
-    }
-    {
+    point n4(4,1.,1.);
+    triangle t1(n1,n2,n3);
+    triangle t2(n2,n3,n4);
+    list<triangle> TT;
+    TT.push_back(t1);
+    TT.push_back(t2);
+    Assemblage A(4);
+    A.assembler(TT);
+    A.MM.print();
+    A.RR.print();
 
-        // Si A est la matrice diagonale(1,2) on doit avoir x = [2 1]
-        cout << "Test Diagonale" << endl;
-        Matrice_creuse A(2,2);
-        Noeud N1(1,1,1.);
-        Noeud N2(2,2,2.);
-        A.get_Chaine().push_back(N1);
-        A.get_Chaine().push_back(N2);
+    cout<<"partie d'assemblage 1 ok"<<endl;
 
-        vector<double> b(2,2.);
-        vector<double> x=grad_conj(A,b);
-        print(x);
+    point n5(5,0.5,0.);
+    point n6(6,0.5,0.5);
+    point n7(7,0.,0.5);
+    Triinte ti2(n1,n2,n3,n5,n6,n7);
+    cout<<ti2.sommets[2].y<<endl;
+    //cout<<"yeah"<<endl;
 
 
+    cout<<ti2.bl<<endl;
+    cout<<ti2.sommets[5].y<<endl;
+    cout<<ti2.type<<endl;
+    ti2.masse();
+    ti2.mprint();
+    ti2.rigid();
+    ti2.rprint();
 
-    }
-    {
+    cout<<ti2.coord[5]<<endl;
+    double testm=integrale_p2_masse(ti2.coord, 5, 6);
+    //double testr=integrale_p2_rigidite(ti2.coord, 5, 6);
+    cout<<testm<<endl;
 
-        // Si A est la matrice diagonale(1,2) on doit avoir x = [2 1]
-        // NE FONCTIONNNE PAS
-        cout << "Test Diagonale strictement dominante" << endl;
-        Matrice_creuse A(2,2);
-        Noeud N1(1,1,1.);
-        Noeud N2(2,2,2.);
-        Noeud N3(1,2,0.5);
-        Noeud N4(2,1,0.5);
-        A.get_Chaine().push_back(N1);
-        A.get_Chaine().push_back(N2);
-        A.get_Chaine().push_back(N3);
-        A.get_Chaine().push_back(N4);
-        A.print();
-
-        vector<double> b(2,2.);
-        vector<double> x=grad_conj(A,b);
-        print(x);
-
-
-        cout << "Fin Test du gradient conjugué" << endl;
+    cout<<"partie d'integrale 2"<<endl;
+    point r1(1,0.,0.);
+    point r2(2,1.,0.);
+    point r3(3,0.5,1.);
+    point r4(4,0.5,0.);
+    point r5(5,0.75,0.5);
+    point r6(6,0.25,0.5);
+    Triinte ti3(r1,r2,r3,r4,r5,r6);
+    //cout<<"yeah"<<endl;
 
 
-    }
+    cout<<ti3.bl<<endl;
+    cout<<ti3.milieux[2].x<<endl;
+    cout<<ti3.type<<endl;
+    double test = integrale_p2_rigidite(ti3.coord,4,5);
+    cout << 'test =' << test << endl;
+    ti3.masse();
+    ti3.mprint();
+    ti3.rigid();
+    ti3.rprint();
 
-    {
-        point r1(1,0.,0.);
-        point r2(2,1.,0.);
-        point r3(3,0.5,1.);
-        point r4(4,0.5,0.);
-        point r5(5,0.75,0.5);
-        point r6(6,0.25,0.5);
+    cout<<ti3.coord[4]<<endl;
+    double testm2=integrale_p2_masse(ti3.coord, 4, 5);
+    double testr2=integrale_p2_rigidite(ti3.coord, 4, 5);
+    cout<<testm2<<endl;
 
-        Triinte ti3(r1,r2,r3,r4,r5,r6);
+    cout<<"partie d'integrale 2 ok"<<endl;
 
-        double test2;
-        test2 = integrale_p2_rigidite(ti3.coord, 5,6);
 
-        //cout << l1.at(0) ;
 
-    }
-        return 0;
+    cout << "It runs!" << endl;
+    return 0;
 }
+
+//on a list<triangle> et Nbp
+//Assemblage AA(Nbp);
+//AA.assembler(TT);
+
