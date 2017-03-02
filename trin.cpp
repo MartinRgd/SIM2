af6ea41fc55dfc30ff5974b95be421c1b7df67ec
ZHONGXIUZHANG <zzxshr@sina.cn>
2017-03-02 12:12:13 +0100

    Add files via upload

--- trin.cpp
+++ trin.cpp
@@ -7,6 +7,7 @@
 #include"matrice_creuse.h"
 #include"trin.h"
 #include"triangles.h"
+#include"intP2.h"
 
 using namespace std;
 
@@ -46,52 +47,77 @@ ostream & operator <<(ostream & out,const Triangle &U)
 */
 void Triinte::masse (double a)
 {
-    Noeud n1(0,0,a*bl/12);
-    Noeud n2(0,1,a*bl/24);
-    Noeud n3(0,2,a*bl/24);
-    Noeud n4(1,0,a*bl/24);
-    Noeud n5(1,1,a*bl/12);
-    Noeud n6(1,2,a*bl/24);
-    Noeud n7(2,0,a*bl/24);
-    Noeud n8(2,1,a*bl/24);
-    Noeud n9(2,2,a*bl/12);
-    M.Chaine.push_back(n1);M.Chaine.push_back(n2);M.Chaine.push_back(n3);
-    M.Chaine.push_back(n4);M.Chaine.push_back(n5);M.Chaine.push_back(n6);
-    M.Chaine.push_back(n7);M.Chaine.push_back(n8);M.Chaine.push_back(n9);
+    if(type=1)
+    {
+        Noeud n1(0,0,a*bl/12);
+        Noeud n2(0,1,a*bl/24);
+        Noeud n3(0,2,a*bl/24);
+        Noeud n4(1,0,a*bl/24);
+        Noeud n5(1,1,a*bl/12);
+        Noeud n6(1,2,a*bl/24);
+        Noeud n7(2,0,a*bl/24);
+        Noeud n8(2,1,a*bl/24);
+        Noeud n9(2,2,a*bl/12);
+        M.Chaine.push_back(n1);M.Chaine.push_back(n2);M.Chaine.push_back(n3);
+        M.Chaine.push_back(n4);M.Chaine.push_back(n5);M.Chaine.push_back(n6);
+        M.Chaine.push_back(n7);M.Chaine.push_back(n8);M.Chaine.push_back(n9);
+
+    }
+    else
+    {
+        for(int i=1;i<7;i++)
+        {
+            for(int j=1;j<7;j++)
+            M.Chaine.push_back(Noeud(i-1,j-1,integrale_p2_masse(coord, i, j)));
+        }
+    }
+
 }
 
 void Triinte::rigid (double b)
 {
-    vector<double> deltaom1(2);
-    deltaom1.at(0)=-1.;deltaom1.at(1)=-1.;
-    vector<double> deltaom2(2);
-    deltaom2.at(0)=1.;deltaom2.at(1)=0.;
-    vector<double> deltaom3(2);
-    deltaom3.at(0)=0.;deltaom3.at(1)=1.;
-    //cout<<"1 ok"<<endl;
-    //Noeud r1(0,0,1.);
-    //R.Chaine.push_back(r1);
-    Matrice_creuse BLTI(2,2);
-    Noeud b1(0,0,(sommets[2].y-sommets[0].y)/bl);Noeud b2(0,1,(sommets[0].x-sommets[2].x)/bl);
-    Noeud b3(1,0,(sommets[0].y-sommets[1].y)/bl);Noeud b4(1,1,(sommets[1].x-sommets[0].x)/bl);
-    BLTI.Chaine.push_back(b1);BLTI.Chaine.push_back(b2);
-    BLTI.Chaine.push_back(b3);BLTI.Chaine.push_back(b4);
-    //ps(deltaom1,deltaom2);
-    //vector<double> test(2);
-    //test=BLTI*deltaom1;
-    //double p=ps(BLTI*deltaom1,BLTI*deltaom1);
-    Noeud r1(0,0,(0.5)*b*ps(BLTI*deltaom1,BLTI*deltaom1));
-    Noeud r2(0,1,(0.5)*b*ps(BLTI*deltaom1,BLTI*deltaom2));
-    Noeud r3(0,2,(0.5)*b*ps(BLTI*deltaom1,BLTI*deltaom3));
-    Noeud r4(1,0,(0.5)*b*ps(BLTI*deltaom2,BLTI*deltaom1));
-    Noeud r5(1,1,(0.5)*b*ps(BLTI*deltaom2,BLTI*deltaom2));
-    Noeud r6(1,2,(0.5)*b*ps(BLTI*deltaom2,BLTI*deltaom3));
-    Noeud r7(2,0,(0.5)*b*ps(BLTI*deltaom3,BLTI*deltaom1));
-    Noeud r8(2,1,(0.5)*b*ps(BLTI*deltaom3,BLTI*deltaom2));
-    Noeud r9(2,2,(0.5)*b*ps(BLTI*deltaom3,BLTI*deltaom3));
-    R.Chaine.push_back(r1);R.Chaine.push_back(r2);R.Chaine.push_back(r3);
-    R.Chaine.push_back(r4);R.Chaine.push_back(r5);R.Chaine.push_back(r6);
-    R.Chaine.push_back(r7);R.Chaine.push_back(r8);R.Chaine.push_back(r9);
+    if(type=1)
+    {
+        vector<double> deltaom1(2);
+        deltaom1.at(0)=-1.;deltaom1.at(1)=-1.;
+        vector<double> deltaom2(2);
+        deltaom2.at(0)=1.;deltaom2.at(1)=0.;
+        vector<double> deltaom3(2);
+        deltaom3.at(0)=0.;deltaom3.at(1)=1.;
+        //cout<<"1 ok"<<endl;
+        //Noeud r1(0,0,1.);
+        //R.Chaine.push_back(r1);
+        Matrice_creuse BLTI(2,2);
+        Noeud b1(0,0,(sommets[2].y-sommets[0].y)/bl);Noeud b2(0,1,(sommets[0].x-sommets[2].x)/bl);
+        Noeud b3(1,0,(sommets[0].y-sommets[1].y)/bl);Noeud b4(1,1,(sommets[1].x-sommets[0].x)/bl);
+        BLTI.Chaine.push_back(b1);BLTI.Chaine.push_back(b2);
+        BLTI.Chaine.push_back(b3);BLTI.Chaine.push_back(b4);
+        //ps(deltaom1,deltaom2);
+        //vector<double> test(2);
+        //test=BLTI*deltaom1;
+        //double p=ps(BLTI*deltaom1,BLTI*deltaom1);
+        Noeud r1(0,0,(0.5)*b*ps(BLTI*deltaom1,BLTI*deltaom1));
+        Noeud r2(0,1,(0.5)*b*ps(BLTI*deltaom1,BLTI*deltaom2));
+        Noeud r3(0,2,(0.5)*b*ps(BLTI*deltaom1,BLTI*deltaom3));
+        Noeud r4(1,0,(0.5)*b*ps(BLTI*deltaom2,BLTI*deltaom1));
+        Noeud r5(1,1,(0.5)*b*ps(BLTI*deltaom2,BLTI*deltaom2));
+        Noeud r6(1,2,(0.5)*b*ps(BLTI*deltaom2,BLTI*deltaom3));
+        Noeud r7(2,0,(0.5)*b*ps(BLTI*deltaom3,BLTI*deltaom1));
+        Noeud r8(2,1,(0.5)*b*ps(BLTI*deltaom3,BLTI*deltaom2));
+        Noeud r9(2,2,(0.5)*b*ps(BLTI*deltaom3,BLTI*deltaom3));
+        R.Chaine.push_back(r1);R.Chaine.push_back(r2);R.Chaine.push_back(r3);
+        R.Chaine.push_back(r4);R.Chaine.push_back(r5);R.Chaine.push_back(r6);
+        R.Chaine.push_back(r7);R.Chaine.push_back(r8);R.Chaine.push_back(r9);
+    }
+    else
+    {
+        for(int i=1;i<7;i++)
+        {
+            for(int j=1;j<7;j++)
+            M.Chaine.push_back(Noeud(i-1,j-1,integrale_p2_rigidite(coord, i, j)));
+        }
+    }
+
 }
 
 void Triinte::mprint ()
--- trin.h
+++ trin.h
@@ -10,6 +10,7 @@
 #include"matrice_creuse.h"
 #include"triangles.h"
 #include"trin.h"
+#include"intP2.h"
 
 using namespace std;
 
@@ -20,18 +21,58 @@ class Triinte: public triangle
     Matrice_creuse M;
     Matrice_creuse R;
     double bl;
+    vector<double> coord;
 
-    Triinte()
+    /*Triinte()
     :triangle()
     {
-        bl=(sommets[1].x-sommets[0].x)*(sommets[2].y-sommets[0].y)-(sommets[2].x-sommets[0].x)*(sommets[1].y-sommets[0].y);
+        switch(type)
+          {
+             case 1:
+             bl=(sommets[1].x-sommets[0].x)*(sommets[2].y-sommets[0].y)-(sommets[2].x-sommets[0].x)*(sommets[1].y-sommets[0].y);
+             break;
+
+             case 2:
+             bl=jacobian_triangle(sommets);
+             break;
+
+          };
+    }*/
+
+    Triinte(const point &P1, const point &P2,const point &P3)
+    :triangle(P1,P2,P3),M(3,3),R(3,3)
+    {
+             bl=(sommets[1].x-sommets[0].x)*(sommets[2].y-sommets[0].y)-(sommets[2].x-sommets[0].x)*(sommets[1].y-sommets[0].y);
 
     }
 
-    Triinte(const point &U, const point &V,const point &W)
-    :triangle(U,V,W),M(3,3),R(3,3)
+    Triinte(const point &P1,const point &P2,const point &P3,const point &P4,const point &P5,const point &P6)
+    :triangle(P1,P2,P3,P4,P5,P6),M(6,6),R(6,6)
     {
-        bl=(sommets[1].x-sommets[0].x)*(sommets[2].y-sommets[0].y)-(sommets[2].x-sommets[0].x)*(sommets[1].y-sommets[0].y);
+             for(int i=0;i<3;i++)
+              {
+                 coord.push_back(sommets[i].x);
+                 coord.push_back(sommets[i].y);
+              }
+             bl=jacobian_triangle(coord);
+
+    }
+    Triinte(const triangle &T)
+    :triangle(T),M(type*3,type*3),R(type*3,type*3)
+    {
+            if (type=1)
+            {
+                bl=(sommets[1].x-sommets[0].x)*(sommets[2].y-sommets[0].y)-(sommets[2].x-sommets[0].x)*(sommets[1].y-sommets[0].y);
+            }
+            else
+            {
+                for(int i=0;i<3;i++)
+                 {
+                   coord.push_back(sommets[i].x);
+                   coord.push_back(sommets[i].y);
+                 }
+                bl=jacobian_triangle(coord);
+            }
 
     }
 
